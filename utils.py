f1d66d64593da89cc0736741b4b997f0de06f11b
MohsenAmiri79 <mmohsenamiri79@yahoo.com>
2023-05-11 19:23:11 +0330

    Initial Commit. Data Cleaning Process: Done.

---
+++ utils.py
@@ -0,0 +1,16 @@
+class dict2w(dict):
+    def __setitem__(self, key, value):
+        if key in self:
+            del self[key]
+        if value in self:
+            del self[value]
+        dict.__setitem__(self, key, value)
+        dict.__setitem__(self, value, key)
+
+    def __delitem__(self, key):
+        dict.__delitem__(self, self[key])
+        dict.__delitem__(self, key)
+
+    def __len__(self):
+        """Returns the number of connections"""
+        return dict.__len__(self) // 2
